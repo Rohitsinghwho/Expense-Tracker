82a596eaaf28cd1fc7f49410ab2fe24f35878ff9
rohitsinghwho <rohiisingh8655@gmail.com>
2025-08-08 02:28:45 +0530

    added space in button

--- src/componet/Button/PrimaryButton.jsx
+++ src/componet/Button/PrimaryButton.jsx
@@ -4,7 +4,7 @@ import "./Button.css"
 const PrimaryButton = (props) => {
   return (
     <button className='btn-primary' style={{background:`linear-gradient(90deg,${props.color1} 0%,${props.color2} 100%`}} onClick={props.onclick}>
-        <span>+{props.name}</span>
+        <span>+ {props.name}</span>
     </button>
   )
 }
